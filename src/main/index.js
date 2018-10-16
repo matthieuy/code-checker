eb610880d9ba95fa9deaf93ad98b2b4eaffb8589
Matthieu <matthieuy@riseup.net>
2018-10-16 20:20:27 +0200

    Bug with portable version : temporary disable

--- src/main/index.js
+++ src/main/index.js
@@ -3,18 +3,18 @@
  */
 'use strict'
 
-import { app } from 'electron'
-let path = require('path')
+// import { app } from 'electron'
+// let path = require('path')
 
 /********************
  * Portable version *
  ********************/
-if (process.env.PORTABLE_EXECUTABLE_DIR) {
-  let portablePath = path.join(process.env.PORTABLE_EXECUTABLE_DIR, 'codechecker_data')
-  app.setPath('userData', path.join(portablePath, 'userdata'))
-  app.setPath('temp', path.join(portablePath, 'tmp'))
-  app.setPath('logs', path.join(portablePath, 'logs'))
-}
+// if (process.env.PORTABLE_EXECUTABLE_DIR) {
+//   let portablePath = path.join(process.env.PORTABLE_EXECUTABLE_DIR, 'codechecker_data')
+//   app.setPath('userData', path.join(portablePath, 'userdata'))
+//   app.setPath('temp', path.join(portablePath, 'tmp'))
+//   app.setPath('logs', path.join(portablePath, 'logs'))
+// }
 
 // Load main
 require('./main')
